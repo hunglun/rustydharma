0a635a96cd89afeae1b57727c09087d0d10137df
O Hung Lun <hunglun.o@gmail.com>
2021-09-18 05:33:46 +0800

    use lalrpop

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
+#[macro_use] extern crate lalrpop_util;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+    
+    #[test]
+    fn calculator1() {
+        assert!(grammar::TermParser::new().parse("22").is_ok());
+        assert!(grammar::TermParser::new().parse("(22)").is_ok());
+        assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
+        assert!(grammar::TermParser::new().parse("((22)").is_err());
+    }
+    
 }
